e11f806926f4a5ff75c9cdedfa9198dbcca29a5c
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2026-03-08 00:32:05 +0530

    Update command.py

--- modules/helpers/command.py
+++ modules/helpers/command.py
@@ -1,9 +1,13 @@
 from typing import Union, List
 from pyrogram import filters
+from modules.config import COMMAND_PREFIXES
 
+# 1. Standard Group Filters
 other_filters = filters.group & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
+
+# 2. Standard Private Filters
 other_filters2 = filters.private & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
 
-
-def commandpro(commands: Union[str, List[str]]):
-    return filters.command(commands,"")
+# 3. Standard Command (uses / or !)
+def command(commands: Union[str, List[str]]):
+    return filters.command(commands, COMMAND_PREFIXES)
