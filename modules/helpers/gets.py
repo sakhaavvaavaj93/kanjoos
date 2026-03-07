f4cbf362b6ef205cce88c052c2ca20d6624f2dc5
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2026-03-08 00:25:18 +0530

    Update gets.py

--- modules/helpers/gets.py
+++ modules/helpers/gets.py
@@ -1,7 +1,6 @@
 from typing import Union
 from pyrogram.types import Message, Audio, Voice
 
-
 def get_url(message_1: Message) -> Union[str, None]:
     messages = [message_1]
 
@@ -23,11 +22,19 @@ def get_url(message_1: Message) -> Union[str, None]:
                     offset, length = entity.offset, entity.length
                     break
 
-    if offset in (None,):
+    if offset is None:
         return None
 
     return text[offset:offset + length]
 
-
+# Replace your old get_file_name with this one:
 def get_file_name(audio: Union[Audio, Voice]):
-    return f'{audio.file_unique_id}.{audio.file_name.split(".")[-1] if not isinstance(audio, Voice) else "ogg"}'
+    if isinstance(audio, Voice):
+        return f'{audio.file_unique_id}.ogg'
+    
+    # Safer extraction: defaults to mp3 if file_name is missing/broken
+    ext = "mp3" 
+    if audio.file_name and "." in audio.file_name:
+        ext = audio.file_name.split(".")[-1]
+        
+    return f'{audio.file_unique_id}.{ext}'
