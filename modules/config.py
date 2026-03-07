00d34a19a1214b99ab8954448e39ce72662a89b2
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2026-03-08 00:06:03 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,7 +1,15 @@
 import os
-import aiohttp
 from os import getenv
 from dotenv import load_dotenv
+import aiohttp
+
+_session = None
+
+async def get_session():
+    global _session
+    if _session is None or _session.closed:
+        _session = aiohttp.ClientSession()
+    return _session
     
 load_dotenv()
 que = {}
